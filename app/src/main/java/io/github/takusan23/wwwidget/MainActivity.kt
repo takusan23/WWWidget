96e8027c0917491c08e73e3e588cd6b3c0419310
takusan23 <takumu1200@gmail.com>
2020-01-30 16:01:01 +0900

    画像保存、ライセンス追加した

--- app/src/main/java/io/github/takusan23/wwwidget/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/wwwidget/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.takusan23.wwwidget
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,9 @@ import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.Jsoup
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var preferences: SharedPreferences
     // GitHubから取得＋HTMLパース＋画像生成　関数があるクラス
     val kusa = Kusa()
+    // 生成したBitmap
+    lateinit var grassBitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +52,33 @@ class MainActivity : AppCompatActivity() {
             kusa(userName)
         }
 
+        // ライセンス画面に
+        license_button.setOnClickListener {
+            val intent = Intent(this, LicenseActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 保存ボタン
+        save_button.setOnClickListener {
+            saveGrass()
+        }
+
+    }
+
+    // 保存する
+    private fun saveGrass() {
+        if (::grassBitmap.isInitialized) {
+            // 初期化済み
+            // 日付
+            val simpleDateFormat = SimpleDateFormat("yyyyMMddhhMMss")
+            val date = Date()
+            // ファイル生成
+            // ScopedStorage
+            val file = File("${externalMediaDirs[0].path}/WWWGrass_${simpleDateFormat.format(date)}.png")
+            file.createNewFile()
+            // 保存する
+            grassBitmap.compress(Bitmap.CompressFormat.PNG, 100, file.outputStream())
+        }
     }
 
     // コルーチンチン！！！
@@ -57,6 +90,8 @@ class MainActivity : AppCompatActivity() {
         // println(colorList)
         val grass = kusa.createGrassCanvas(colorList)
         imageview.setImageBitmap(grass)
+        // 保存用に
+        grassBitmap = grass
     }
 
 
